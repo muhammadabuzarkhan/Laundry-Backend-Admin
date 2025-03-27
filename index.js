f8be05e242daea7d6ff6d6b04bd4c6a8712b76aa
Muhammad Abuzar <116755569+muhammadabuzarkhan@users.noreply.github.com>
2025-03-27 06:43:08 +0500

    Lamda

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-//imports
+// app.js (the main file you provided)
 const express = require("express");
 const bodyParser = require('body-parser');
 const rateLimit = require("express-rate-limit");
 const cors = require("cors");
-const https = require("https");
-const http = require("http");
-const credentials = require("./ssl");
 const morgan = require("morgan");
 const swagger = require("./Swagger");
 
 require("dotenv").config();
-const { NODE_ENV, PORT } = process.env
+const { NODE_ENV, PORT } = process.env;
 
 const app = express();
 
@@ -26,13 +23,14 @@ const coreOptions = {
     exposedHeaders:
         "Content-Type, Authorization, X-Requested-With, Accept, VERSION",
 };
+
 app.use(cors(coreOptions));
 app.use(morgan("dev"));
 app.use(express.json({ limit: '550mb' }));
 app.use(express.urlencoded({ extended: true }));
-app.use('/swagger', swagger.serve, swagger.setup)
+app.use('/swagger', swagger.serve, swagger.setup);
 
-//limiting the api calls
+// limiting the API calls
 const limiter = rateLimit({
     max: 1000000,
     windowMs: 60 * 60 * 1000,
@@ -41,22 +39,15 @@ const limiter = rateLimit({
 
 app.use("/api", limiter);
 
-//static routes
+// static routes
 app.use("/Uploads", express.static("./Uploads"));
 
 // routes register
 app.use("/api", require("./Routes/index"));
 
 app.get("/", (req, res) => {
-    console.log("NODE_ENV",NODE_ENV);
+    console.log("NODE_ENV", NODE_ENV);
     res.send(`Laundry wash Server is Running on ${NODE_ENV}`);
 });
 
-var httpsServer;
-if (NODE_ENV === "development") httpsServer = http.createServer(app)
-else httpsServer = https.createServer(credentials, app)
-
-httpsServer.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
-});
-
+module.exports = app; // Export app to use in lambda.js
